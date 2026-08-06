51c61aafeb6f84458084d5cf16ff3b6519c81271
Nicholas Tangunan <nikkotangunan@gmail.com>
2026-08-06 14:09:26 -0700

    implemented non-blocking LED timing with millis() and refactored code to allow for modular design for additional devices

--- firmware/src/app/application.cpp
+++ firmware/src/app/application.cpp
@@ -1,13 +1,19 @@
 #include "application.h"
 #include <Arduino.h>
 
+#include "../config/board_config.h"
 #include "../devices/button/button.h"
 #include "../devices/led/led.h"
+
+// timing control variables (unsigned long)
+unsigned long prevMillis = 0;
+const unsigned long interval = 500; // interval duration: 500 ms (0.5 s)
 
 // Define the devices in an anonymous namespace (file-scope)
 namespace
 {
-    Devices::LED led;
+    Devices::LED buttonLed(Board::STATUS_LED_PIN);
+    Devices::LED heartbeatLed(Board::TIMER_LED_PIN);
     Devices::Button button;
 }
 
@@ -15,23 +21,31 @@ namespace App
 {
     void Application::initialize()
     {
-        led.initialize();
+        buttonLed.initialize();
+        heartbeatLed.initialize();
         button.initialize();
     }
     
     void Application::update()
     {
+        // button pressed -> blue toggled
         bool pressed = button.isPressed();
-        
-        Serial.println(pressed);
 
         if (pressed)
         {
-            led.on();
+            buttonLed.on();
         }
         else
         {
-            led.off();
+            buttonLed.off();
+        }
+
+        // capture time -> red toggled
+        unsigned long currMillis = millis();
+        
+        if (currMillis - prevMillis >= interval) {
+            prevMillis = currMillis;
+            heartbeatLed.toggle();
         }
     }
 }
--- firmware/src/config/board_config.h
+++ firmware/src/config/board_config.h
@@ -5,6 +5,11 @@
 // job is to describe hardware
 namespace Board
 {
+    // blue led toggled by button
     constexpr uint8_t STATUS_LED_PIN = 2;
     constexpr uint8_t USER_BUTTON_PIN = 15;
+
+    // red led toggled by timing
+    constexpr uint8_t TIMER_LED_PIN = 0;
+    
 }
