c1212946527408a54104012de0d4fd08c568d001
Nicholas Tangunan <nikkotangunan@gmail.com>
2026-08-27 01:33:55 -0700

    trying to fix callbacks

--- firmware/src/app/application.cpp
+++ firmware/src/app/application.cpp
@@ -6,46 +6,68 @@
 #include "../devices/button/button.h"
 #include "../devices/led/led.h"
 
-// timing control variables (unsigned long)
-unsigned long heartbeatLastRun = 0;
-const unsigned long heartbeatInterval = 500; // interval duration: 500 ms (0.5 s)
+// unsigned long statusLastRun = 0;
+// const unsigned long statusInterval = 1000; // interval duration: 1000 ms (1 s)
 
-unsigned long statusLastRun = 0;
-const unsigned long statusInterval = 1000; // interval duration: 1000 ms (1 s)
-
-// Define the devices and scheduler in an anonymous namespace (file-scope)
+// anonymous namespace (file-scope)
 namespace
 {
+    // --------------------------------------------------
+    // Devices
+    // --------------------------------------------------
     Devices::LED buttonControlledLed(Board::BUTTON_CONTROLLED_LED_PIN);
     Devices::LED heartbeatLed(Board::HEARTBEAT_LED_PIN);
     Devices::LED fastFlashLED(Board::FAST_FLASH_LED_PIN);
     Devices::Button button;
+
+    // --------------------------------------------------
+    // Callbacks (converting lambda to a func ptr whose func returns void and takes no arguments)
+    // --------------------------------------------------
+
+    void (*heartbeatLedCallback)() = []()
+        {
+            heartbeatLed.toggle();
+        };
+
+    void (*fastFlashCallback)() = []()
+        {
+            fastFlashLED.toggle();
+        };
+
+    void (*statusCallback)() = []()
+        {
+            Serial.println("Firmware alive");
+        };
+    
+
+    // --------------------------------------------------
+    // Scheduler
+    // --------------------------------------------------
     Timing::Scheduler scheduler;
+
+    // --------------------------------------------------
+    // Tasks
+    // --------------------------------------------------
+
+    Timing::Scheduler::Task heartbeatTask {
+        heartbeatLedCallback,
+        500, // interval duration: 500 ms (0.5 s)
+        0
+    };
 }
 
 namespace App
 {
     void Application::initialize()
     {
+        // devices
         buttonControlledLed.initialize();
         heartbeatLed.initialize();
         fastFlashLED.initialize();
         button.initialize();
 
-        auto heartbeatLedCallback = []()
-        {
-            heartbeatLed.toggle();
-        };
-
-        auto fastFlashCallback = []()
-        {
-            fastFlashLED.toggle();
-        };
-
-        auto statusCallback = []()
-        {
-            Serial.println("Firmware alive");
-        };
+        // scheduler tasks
+        scheduler.addTask(heartbeatTask);
     }
     
     void Application::update()
