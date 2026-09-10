63641c0747ba3fdcf1588517437735a89fd0d3b7
Nicholas Tangunan <nikkotangunan@gmail.com>
2026-09-10 12:59:09 -0700

    moved pwm_resource_manager related code to its own module under pwm

--- firmware/src/pwm/pwm.h
+++ firmware/src/pwm/pwm.h
@@ -1,45 +1,9 @@
 #pragma once
 
-#include "../config/board_config.h"
-#include <array>
-
 struct PWMConfig
 {
     int channel;
     int resolution;
-};
-
-struct PWMResource
-{
-    int resourceId;
-    bool isAllocated;
-    int allocationId;
-    int pin;
-    int frequency;
-    int resolution;
-};
-
-struct PWMRequirements
-{
-    int pin;
-    int frequency;
-    int resolution;
-};
-
-struct PWMAllocationHandle
-{
-    int resourceId;
-    int allocationId;
-};
-
-struct PWMResourceManager
-{
-    std::array<PWMResource, Board::MAX_PWM_RESOURCES> resources; // collection of PWM resources
-    int nextAllocationId;
-    int updateNextAllocationId();
-    PWMAllocationHandle allocate(PWMRequirements requirements);
-    bool validate(PWMAllocationHandle handle);
-    void release(PWMAllocationHandle handle);
 };
 
 class PWM
---
+++ firmware/src/pwm/pwm_resource_manager.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "../config/board_config.h"
+#include <array>
+
+struct PWMResource
+{
+    int resourceId;
+    bool isAllocated;
+    int allocationId;
+    int pin;
+    int frequency;
+    int resolution;
+};
+
+struct PWMRequirements
+{
+    int pin;
+    int frequency;
+    int resolution;
+};
+
+struct PWMAllocationHandle
+{
+    int resourceId;
+    int allocationId;
+};
+
+struct PWMResourceManager
+{
+    std::array<PWMResource, Board::MAX_PWM_RESOURCES> resources; // collection of PWM resources
+    int nextAllocationId;
+    int updateNextAllocationId();
+    PWMAllocationHandle allocate(PWMRequirements requirements);
+    bool validate(PWMAllocationHandle handle);
+    void release(PWMAllocationHandle handle);
+};
