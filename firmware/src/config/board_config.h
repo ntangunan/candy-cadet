12a1906807f0963222b71efee88013b8a1870926
Nicholas Tangunan <nikkotangunan@gmail.com>
2026-09-16 23:28:28 -0700

    updated board config with new pins

--- firmware/src/config/board_config.h
+++ firmware/src/config/board_config.h
@@ -10,29 +10,27 @@ namespace Board
 
     // led toggled by button
     constexpr uint8_t BUTTON_CONTROLLED_LED_PIN = 32;
-    constexpr uint8_t USER_BUTTON_PIN = 12;
+    constexpr uint8_t USER_BUTTON_PIN = 33;
 
     // led toggled by timing
-    constexpr uint8_t HEARTBEAT_LED_PIN = 33;
+    constexpr uint8_t HEARTBEAT_LED_PIN = 4;
 
     // led toggled by callback
     constexpr uint8_t FAST_FLASH_LED_PIN = 13;
 
     // led with brightness controlled by pwm
-    constexpr uint8_t PWM_LED_PIN_0 = 19;
-
-    // led with brightness controlled by pwm
-    constexpr uint8_t PWM_LED_PIN_1 = 18;
+    constexpr uint8_t PWM_LED_PIN_0 = 18;
 
     // servo controlled by pwm
-    constexpr uint8_t PWM_SERVO_PIN_0 = 25;
+    constexpr uint8_t PWM_SERVO_PIN_0 = 19;
 
     // tt motors controlled by pwm 
+    // left motor A
+    constexpr uint8_t LEFT_MOTOR_A_CONTROL_PIN_1 = 25;
+    constexpr uint8_t LEFT_MOTOR_A_CONTROL_PIN_2 = 26;
 
-    constexpr uint8_t LEFT_MOTOR_CONTROL_PIN_1 = 25;
-    constexpr uint8_t LEFT_MOTOR_CONTROL_PIN_2 = 26;
-
-    constexpr uint8_t RIGHT_MOTOR_CONTROL_PIN_1 = 27;
-    constexpr uint8_t RIGHT_MOTOR_CONTROL_PIN_2 = 14;
+    // right motor B
+    constexpr uint8_t RIGHT_MOTOR_B_CONTROL_PIN_1 = 27;
+    constexpr uint8_t RIGHT_MOTOR_B_CONTROL_PIN_2 = 14;
 
 }
